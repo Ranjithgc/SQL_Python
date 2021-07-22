88e7e8ac08c45104e373e04d6497b2815abe4fbd
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-07-22 22:33:35 +0530

    [Ranjithgc] Add: estableshed connection with mysql

---
+++ SubQueries/SuQueries.py
@@ -0,0 +1,50 @@
+"""
+@Author: Ranjith G C
+@Date: 2021-07-22
+@Last Modified by: Ranjith G C
+@Last Modified time: 2021-07-22 
+@Title : Program Aim is to work with Subqueries.
+"""
+
+import os
+import mysql.connector
+from dotenv import load_dotenv
+from loggers import logger
+
+load_dotenv()
+
+class SubQuery:
+   
+    def __init__(self):
+        '''
+        Description:
+            created constructor and test mysql database connection.
+        Parameter:
+            it takes self as parameter.
+        '''
+        self.db_connection = mysql.connector.connect(
+            host=os.getenv('HOST'),
+            user=os.getenv('USER'),
+            passwd=os.getenv('PASSWD'),
+            auth_plugin=os.getenv('AUTH_PLUGIN')
+        )
+        self.db_cursor = self.db_connection.cursor()
+
+    def print_connection(self):
+        '''
+        Description:
+            this function prints the connection object.
+        Parameter:
+            it takes self as parameter.
+        '''
+        
+        try:
+            logger.info(self.db_connection)
+            self.db_cursor.execute("USE ARUN")
+        
+        except Exception as e:
+            logger.error(e)
+
+if __name__ == "__main__":
+    query = SubQuery()
+    query.print_connection()
